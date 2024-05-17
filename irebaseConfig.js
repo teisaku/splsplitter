8aa8c840175435086856bfa2dc3a19b2a731d385
teisaku <teisaku.work@gmail.com>
2024-05-17 10:52:04 +0900

    Create irebaseConfig.js

---
+++ irebaseConfig.js
@@ -0,0 +1,14 @@
+// Replace with your Firebase project's configuration
+var firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
